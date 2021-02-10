99ce468d4e0f85bf3dc6c1269b30feadd1980073
Oliwier Borozynski <78812304+OliwierBorozynski@users.noreply.github.com>
2021-02-10 20:48:34 +0000

    Little update
    I added pacman and parrot.live just to test some options

--- menu.sh
+++ menu.sh
@@ -10,13 +10,15 @@ do
 |_|  |_|_____|_| \_|\___/   \___/|_____|____/____/ \___/|_| \_/_/   \_\
                                                                        "
 
+echo "                                  "
 echo "Remember to run this script as root"
 
 echo "                                  "
 
-
-echo "1) start wlan0   2) start wlan0mon  3) restart Network Manager  
-4) stop wlan0"
+echo "1) start wlan0                    5) ls"
+echo "2) start wlan0mon                 6) dancing parrot"
+echo "3) restar NetworkManager          7) install pacman"
+echo "4) stop wlan0                     8) pacman"
 
         read answer
 
@@ -39,6 +41,30 @@ echo "1) start wlan0   2) start wlan0mon  3) restart Network Manager
                 4)
         airmon-ng stop wlan0
         x=1
-        ::
+        ;;
+
+                5)
+        ls
+        x=1
+        ;;
+
+                6)
+        curl parrot.live
+        x=1
+        ;;
+
+
+                7)
+        apt install pacman 
+        x=1
+        ;;
+
+
+                8)
+        clear
+        pacman
+        x=1
+        ;;
+
 esac
 done
