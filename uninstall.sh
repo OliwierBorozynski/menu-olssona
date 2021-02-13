33ffcb07d656161e2288376c97c2eb07c2baeabe
Oliwier Borozynski <78812304+OliwierBorozynski@users.noreply.github.com>
2021-02-13 01:20:49 +0000

    Updating uninstall file again
    ...

--- uninstall.sh
+++ uninstall.sh
@@ -3,23 +3,42 @@ x=0
 while [ $x = 0 ]
 do
 
-echo "Do u want to really uninstall it?"
+echo "Do you really want to uninstall it?"
 echo "1=No 2=Yes"
 
-	read answer
-	case "$answer" in
+        read answer
+        case "$answer" in
 
-	1)
+        1)
         clear
-	x=1
+        x=1
         ;;
 
 
-	2)
+        2)
+
+        echo "Are you sure that you want to uninstall script?"
+        echo "Yes/No"
+
+        read 1answer
+        case "$1answer" in
+
+        No)
+        clear
+        x=1
+        ;;
+
+        Yes)
+        echo "Okay it was nice to meet you :("
+        sleep 7
         rm menu.sh
-	rm uninstall.sh
-	x=1
+        rm uninstall.sh
+        x=1
         ;;
+
+
+esac
+;;
 
 esac
 done
