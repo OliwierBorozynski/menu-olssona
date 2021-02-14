0dbfd81ea238bd5e4a5cc39a94bcda098b3c6747
Oliwier Borozynski <78812304+OliwierBorozynski@users.noreply.github.com>
2021-02-14 04:32:16 +0000

    Update uninstall.sh
    uninstalling install.sh now haha

--- uninstall.sh
+++ uninstall.sh
@@ -1,20 +1,28 @@
 #$bin/bash
+
+
+# ustewienia do zakanczania kazdej z komend aby nie tworzyl sie loop
 x=0
 while [ $x = 0 ]
 do
 
+
+# napisy wyswietlane jako tekst startowy
 echo "Do you really want to uninstall it?"
 echo "1=No 2=Yes"
 
+
+# przypadek numer 1
         read answer
         case "$answer" in
 
         1)
         clear
+        exit
         x=1
         ;;
 
-
+# po wybraniu opcji 2 przypadek numer 2
         2)
 
         echo "Are you sure that you want to uninstall script?"
@@ -25,14 +33,17 @@ echo "1=No 2=Yes"
 
         No)
         clear
+        exit
         x=1
         ;;
 
         Yes)
-        echo "Okay it was nice to meet you :("
         sleep 7
         rm menu.sh
+        rm install.sh
         rm uninstall.sh
+        echo "Okay it was nice to meet you :("
+        sleep 5
         x=1
         ;;
 
