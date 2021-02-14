b69f1c2a0fcc0566a2af9f64f21a37c7ebc14339
Oliwier Borozynski <78812304+OliwierBorozynski@users.noreply.github.com>
2021-02-14 04:35:36 +0000

    menu.sh 1.0
    Im actually happy with menu and im posting it as menu.sh 1.0 version u can see changes that i made

--- menu.sh
+++ menu.sh
@@ -1,7 +1,12 @@
 #$bin/bash
+
+
+# ustewienia do zakanczania kazdej z komend aby nie tworzyl sie loop
 x=0
 while [ $x = 0 ]
-do 
+do
+
+# napisy w menu wyswietlane na ekranie
         clear
         echo " __  __ _____ _   _ _   _    ___  _     ____ ____   ___  _   _    _    
 |  \/  | ____| \ | | | | |  / _ \| |   / ___/ ___| / _ \| \ | |  / \   
@@ -15,21 +20,22 @@ echo "Remember to run this script as root"
 
 echo "                                  "
 
-echo "1) start wlan0                    5) ls"
-echo "2) start wlan0mon                 6) dancing parrot"
-echo "3) restar NetworkManager          7) install pacman"
-echo "4) stop wlan0                     8) pacman"
+echo "1) Update                              5) ip"
+echo "2) Upgarde                             6) dancing parrot"
+echo "3) restar NetworkManager               7) network tools (more options)"
+echo "4) check your available extensions     8) play pacman"
 
+# glowny przypadek
         read answer
 
         case "$answer" in
                 1)
-        airmon-ng start wlan0
+        apt-get update
         x=1
         ;;
 
                 2)
-        airmon-ng start wlan0mon
+        apt-get upgarde
         x=1
         ;;
 
@@ -39,12 +45,12 @@ echo "4) stop wlan0                     8) pacman"
         ;;
 
                 4)
-        airmon-ng stop wlan0
+        iwconfig
         x=1
         ;;
 
                 5)
-        ls
+        ip add
         x=1
         ;;
 
@@ -53,18 +59,31 @@ echo "4) stop wlan0                     8) pacman"
         x=1
         ;;
 
-
-                7)
-        apt install pacman 
-        x=1
-        ;;
-
-
-                8)
+        8)
         clear
         pacman
         x=1
         ;;
+
+
+
+        # network tools
+                7)
+        echo "Choose tool you want to use"
+        echo "1) wifite"
+
+        read answer2
+
+        case "$answer2" in
+
+        1)
+        wifite
+        x=1
+        ;;
+
+
+esac
+;;
 
 esac
 done
